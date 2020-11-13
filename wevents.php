85cdddefd31246dfdedf0e4ca37e090f609a0dea
Abdullahfoysol <73920530+Abdullahfoysol@users.noreply.github.com>
2020-11-13 17:59:01 +0600

    3rd

---
+++ wevents.php
@@ -0,0 +1,11 @@
+<center><b>Weekend events:<b><br><br>
+
+Debate competiton(E/H)<br>
+
+Poetry inter university competiton<br>
+
+Basket ball competition<br></center>
+<!--<img src="images/images.jpg"> -->
+
+ <!--<img src="images/images.jpg" border="1" height="auto" width="auto" position=""> -->
+ <center><img src="images/images.jpg" alt="centered images" height="200" width="398"> </center>
